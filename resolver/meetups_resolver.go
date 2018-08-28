5616bcbe8da372ab401ed380a86eea2961995d28
Scott Kao <scottxxx666@gmail.com>
2018-08-29 02:12:06 +0800

    Add meetups query

--- resolver/meetups_resolver.go
+++ resolver/meetups_resolver.go
@@ -4,9 +4,20 @@ import (
 	"github.com/scottxxx666/meetups-api/service/meetupservice"
 )
 
+// HotMeetups resolve query hotMeetups
 func (r *Resolver) HotMeetups() []*MeetupResolver {
 	var mrs []*MeetupResolver
 	meetups := meetupservice.GetHotMeetups()
+	for _, m := range meetups {
+		mrs = append(mrs, &MeetupResolver{&meetup{m}})
+	}
+	return mrs
+}
+
+// Meetups resolve query Meetups
+func (r *Resolver) Meetups() []*MeetupResolver {
+	var mrs []*MeetupResolver
+	meetups := meetupservice.Get()
 	for _, m := range meetups {
 		mrs = append(mrs, &MeetupResolver{&meetup{m}})
 	}
--- service/meetupservice/meetup_service.go
+++ service/meetupservice/meetup_service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/scottxxx666/meetups-api/model"
 )
 
+// Find meetup by ID
 func Find(id uint64) model.Meetup {
 	var meetup model.Meetup
 	if app.DB.Preload("Level").Preload("Location").Preload("Tags").Preload("Organization").First(&meetup, id).RecordNotFound() {
@@ -14,7 +15,18 @@ func Find(id uint64) model.Meetup {
 	return meetup
 }
 
+// GetHotMeetups get the hot meetups
 func GetHotMeetups() []model.Meetup {
+	var meetups []model.Meetup
+	if app.DB.Preload("Level").Preload("Location").Preload("Tags").Preload("Organization").Limit(3).Find(&meetups).RecordNotFound() {
+		panic("no this meetup")
+	}
+
+	return meetups
+}
+
+// Get get the meetups
+func Get() []model.Meetup {
 	var meetups []model.Meetup
 	if app.DB.Preload("Level").Preload("Location").Preload("Tags").Preload("Organization").Limit(3).Find(&meetups).RecordNotFound() {
 		panic("no this meetup")
